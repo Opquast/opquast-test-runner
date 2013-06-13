8ac3ac7ba05007418d1604ecb2fa7a621b2d07d3
Fabrice Bonny <fabrice.bonny@temesis.com>
2013-06-13 17:46:59 +0200

    Nouveau lot de debug

--- data/lib/oqs-validator.js
+++ data/lib/oqs-validator.js
@@ -1525,7 +1525,9 @@ var logger;
                             'rdfs': 'http://www.w3.org/2000/01/rdf-schema#',
                             'rdf': 'http://www.w3.org/1999/02/22-rdf-syntax-ns#',
                             'adms': 'http://www.w3.org/ns/adms#',
-                            'ecodp': 'http://ec.europa.eu/open-data/ontologie/ec-odp#'
+                            'ecodp': 'http://ec.europa.eu/open-data/ontologie/ec-odp#',
+                            'skos': 'http://www.w3.org/2004/02/skos/core#',
+                            'owl': 'http://www.w3.org/2002/07/owl#'
                         };
                         return ns[prefix] || null;
                     }
