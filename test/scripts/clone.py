69595a432c91e905bf229d185551286a76c259ca
Fabrice Bonny <fabrice.bonny@temesis.com>
2013-06-18 17:49:25 +0200

    Nouveau lot de debug

--- test/scripts/clone.py
+++ test/scripts/clone.py
@@ -3,46 +3,30 @@ from os import listdir
 from json import load
 
 json_criteria = load(open('test/scripts/criteria.json'))
-id = "11576"
-number = 20
+id = "19308"
 
 for dir in listdir('test/fixtures'):
     if dir not in ['rules', 'rules-debug', 'rules-stock', 'rulesets', 'rulesets-debug', 'rulesets-stock'] and dir != id:
-        try:
-            [copy ('test/fixtures/%s/%s-%s_%s.html' % (id,
-                                                       json_criteria[id].replace('.', '-').replace('[', '').replace(']', ''),
-                                                       id,
-                                                       x + 1),
-                   'test/fixtures/%s/%s-%s_%s.html' % (dir,
-                                                       json_criteria[dir].replace('.', '-').replace('[', '').replace(']', ''),
-                                                       dir,
-                                                       x + 1)
-                   ) for x in range(number)]
-        except:
-            pass
-        
-        try:
-            [copy ('test/fixtures/%s/%s-%s_%s.css' % (id,
-                                                       json_criteria[id].replace('.', '-').replace('[', '').replace(']', ''),
-                                                       id,
-                                                       x + 1),
-                   'test/fixtures/%s/%s-%s_%s.css' % (dir,
-                                                       json_criteria[dir].replace('.', '-').replace('[', '').replace(']', ''),
-                                                       dir,
-                                                       x + 1)
-                   ) for x in range(number)]
-        except:
-            pass
-
-        try:
-            [copy ('test/fixtures/%s/%s-%s_%s.json' % (id,
-                                                       json_criteria[id].replace('.', '-').replace('[', '').replace(']', ''),
-                                                       id,
-                                                       x + 2),
-                   'test/fixtures/%s/%s-%s_%s.json' % (dir,
-                                                       json_criteria[dir].replace('.', '-').replace('[', '').replace(']', ''),
-                                                       dir,
-                                                       x + 2)
-                   ) for x in range(number)]
-        except:
-            pass
+        for x in range(20):
+            _id = json_criteria[id].replace('.', '-').replace('[', '').replace(']', '')
+            _dir = json_criteria[dir].replace('.', '-').replace('[', '').replace(']', '').lower()
+            
+            try:
+                copy ('test/fixtures/%s/%s-%s_%s.html' % (id, _id, id, x + 1), 'test/fixtures/%s/%s-%s_%s.html' % (dir, _dir, dir, x + 1))
+            except:
+                pass
+            
+            try:
+                copy ('test/fixtures/%s/%s-%s_%s.css' % (id, _id, id, x + 1), 'test/fixtures/%s/%s-%s_%s.css' % (dir, _dir, dir, x + 1))
+            except:
+                pass
+            
+            try:
+                copy ('test/fixtures/%s/%s-%s_%s.js' % (id, _id, id, x + 1), 'test/fixtures/%s/%s-%s_%s.js' % (dir, _dir, dir, x + 1))
+            except:
+                pass
+            
+            try:
+                copy ('test/fixtures/%s/%s-%s_%s.json' % (id, _id, id, x + 1), 'test/fixtures/%s/%s-%s_%s.json' % (dir, _dir, dir, x + 1))
+            except:
+                pass
