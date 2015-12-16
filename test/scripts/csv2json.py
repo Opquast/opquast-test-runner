473b7468a985b454be93c006af2add4e4aa98003
Fabrice Bonny <fabrice.bonny@temesis.com>
2015-12-16 13:49:22 +0100

    Purge des anciennes listes de Renow

--- test/scripts/csv2json.py
+++ test/scripts/csv2json.py
@@ -6,7 +6,7 @@ from os import getcwd
 from os.path import join
 
 
-csv_file = 'RGAA3.csv'
+csv_file = '728-Renow3.5.csv'
 id = 716
 json_fr = {}
 json_en = {}
@@ -18,16 +18,16 @@ with open(join(getcwd(), 'test/scripts', csv_file), 'r') as fp:
 
     for row in reader(fp, delimiter=','):
         json_fr[row[0].strip()] = {
-            'goal':row[5].strip(),
+            'goal':row[6].strip(),
             'checklist':{
                  'id':id,
-                 'name': u'RGAA V3'
+                 'name': u'Renow 3.5'
             },
             'explanation':'',
-            'description':row[2].strip(),
-            'solution':row[3].strip(),
-            'thema':row[12].strip(),
-            'name':row[1].strip()
+            'description':row[3].strip(),
+            'solution':row[4].strip(),
+            'thema':row[13].strip(),
+            'name':row[2].strip()
         }
 #         json_en[row[0].strip()] = {
 #             'goal':row[11].strip(),
